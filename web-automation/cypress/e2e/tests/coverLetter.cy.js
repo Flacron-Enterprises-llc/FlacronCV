81743689b01b75f14b86462a01ef848defcc65cc
Hina Sarmad <56060132+Hina-Sarmad-SQA@users.noreply.github.com>
2026-04-13 17:44:15 +0500

    Update coverLetter.cy.js

--- web-automation/cypress/e2e/tests/coverLetter.cy.js
+++ web-automation/cypress/e2e/tests/coverLetter.cy.js
@@ -130,11 +130,12 @@ describe('FlacronCV - Create Cover Letter Automation', () => {
           logoutObj.logoutMain()
     });
 
- it('TC: 04: Enter Title and company name and job title and Validate AI genarted button',{ retries: 2 }, () => {
+ it('TC: 04: Enter Title and company name and job title and Validate AI genarted button error for job title',{ retries: 2 }, () => {
              
+              
         clObj.enterTitle(faker.person.jobTitle())
         clObj.enterCompanyName(faker.company.buzzNoun())
-        clObj.enterJobTitle(faker.person.jobTitle())
+
         clObj.clickGenerateWithAI()
         clObj.erroAIgeneratedButt()
         cy.wait(300)
